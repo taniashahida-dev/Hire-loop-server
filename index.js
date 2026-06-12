ae24936ecf598d5f8d848b9fd5ef98eb407c59e2
taniashahida-dev <taniashahida993@gmail.com>
2026-06-12 12:09:19 +0600

    Connect with mongodb

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
+// const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 const uri = process.env.MONGODB_URI;
 
 app.use(cors());
@@ -14,6 +14,38 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('server is running')
 })
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
+async function run() {
+  try {
+  
+     const db = client.db("hire-loop-cullection");
+    const postCullection = db.collection("jobs");
+
+
+
+   
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
